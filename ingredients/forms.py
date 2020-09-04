b9f5d230da4b6713ee185632a31780744c2b2489
Han439 <hanafi4399@gmail.com>
2020-09-04 13:31:21 +0700

    update front-end

--- ingredients/forms.py
+++ ingredients/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class ByIngredients(forms.Form):
-	ingredients = forms.CharField(max_length=200, help_text='What Ingredients Do You Have? (seperate by space)')
+	ingredients = forms.CharField(max_length=200)
--- ingredients/views.py
+++ ingredients/views.py
@@ -24,40 +24,39 @@ def all_recipes(request):
 			# config API before sending
 			uri = 'https://api.spoonacular.com/recipes/findByIngredients'
 			params = {'apiKey': keys, 'ingredients': ingredients_list, 'number': '5'}
-
+			
 			# get the API response
 			response = requests.get(uri, params=params)
 			recipes = response.json()
 
 			# get link to to recipe
 				
-			recipe_param = {'apiKey': keys}
-			#recipe_price = []
+			# recipe_param = {'apiKey': keys}
 
-			for recipe_num in range(len(recipes)):
-				recipe_id = recipes[recipe_num]['id']
-				url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
-				res = requests.get(url, params=recipe_param).json()
-				if 'spoonacularSourceUrl' in res.keys():
-					sourceUrl = res['spoonacularSourceUrl']
-				else:
-					sourceUrl = res['sourceUrl']
+			# for recipe_num in range(len(recipes)):
+			# 	recipe_id = recipes[recipe_num]['id']
+			# 	url = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+			# 	res = requests.get(url, params=recipe_param).json()
+			# 	if 'spoonacularSourceUrl' in res.keys():
+			# 		sourceUrl = res['spoonacularSourceUrl']
+			# 	else:
+			# 		sourceUrl = res['sourceUrl']
 
-				recipes[recipe_num]['sourceUrl'] = sourceUrl
-
-				# Get price and amount
-				#price_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/priceBreakdownWidget.json'
-				#price = requests.get(price_endpoint, params=recipe_param).json()
-				#recipe_price.append(price)
+			# 	recipes[recipe_num]['sourceUrl'] = sourceUrl
 		else:
 			recipes = []
 
 	# pack up datas send for templates render
-	context = {'recipes': recipes, 'form': form}
+	context = {
+			'recipes': recipes,
+			'form': form,
+			}
 
 	return render(request, 'ingredients/recipes.html', context)
 
 def recipe_detail(request, recipe_id):
+
+	# recipe steps
 	recipe_detail_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/analyzedInstructions'
 	
 	params = {'apiKey': keys, "defaultCss":"true"}
@@ -69,23 +68,24 @@ def recipe_detail(request, recipe_id):
 	else:
 		steps = ['There is no recipe yet!']
 
+	# recipe ingredients
+	info_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/information'
+	info = requests.get(info_endpoint, params={'apiKey': keys}).json()
+
 	# get Ingredients Price Break Down Widget
 	widget_header = {'accept': 'text/html'}
 
-	price_widget_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/priceBreakdownWidget'
-	price_widget = requests.get(price_widget_endpoint, params=params, headers=widget_header).text
-
-	price_json_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/priceBreakdownWidget.json'
-	price_json = requests.get(price_json_endpoint, params={'apiKey': keys}).json()
+	# price_json_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/priceBreakdownWidget.json'
+	# price_json = requests.get(price_json_endpoint, params={'apiKey': keys}).json()
 
 	# get Nutrition Break Down Widget
 	nutrition_endpoint = f'https://api.spoonacular.com/recipes/{recipe_id}/nutritionWidget'
 	nutrition_widget = requests.get(nutrition_endpoint, params=params, headers=widget_header).text
 
 	context = {
+		'image': f'https://spoonacular.com/recipeImages/{recipe_id}-556x370.jpg',
 		'steps': steps,
-		'price_widget': price_widget,
-		'price_json': price_json,
+		'info' : info,
 		'nutrition_widget': nutrition_widget
 	}
 
